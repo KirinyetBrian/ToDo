28494e6b9eeee5b513be7fe1fc2fc36917d58326
Kibet Theophilus <kibettheophilus@gmail.com>
2021-03-09 09:56:07 +0300

    Added an adapter

--- app/src/main/java/ke/co/topup/todo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/ke/co/topup/todo/ui/tasks/TasksFragment.kt
@@ -1,13 +1,37 @@
 package ke.co.topup.todo.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import ke.co.topup.todo.R
+import ke.co.topup.todo.databinding.FragmentTasksBinding
 
 @AndroidEntryPoint
 class TasksFragment: Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val tasksAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = tasksAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+        viewModel.tasks.observe(viewLifecycleOwner){
+            tasksAdapter.submitList(it)
+        }
+    }
 }
--- app/src/main/java/ke/co/topup/todo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/ke/co/topup/todo/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,12 @@ package ke.co.topup.todo.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import ke.co.topup.todo.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
 }
