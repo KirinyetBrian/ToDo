1d334e4b028cf3eca12dd6c94a0350399ac33033
Kibet Theophilus <kibettheophilus@gmail.com>
2021-03-10 12:13:47 +0300

    Implemented Search functionality

--- app/src/main/java/ke/co/topup/todo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/ke/co/topup/todo/ui/tasks/TasksFragment.kt
@@ -1,7 +1,11 @@
 package ke.co.topup.todo.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
@@ -10,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import ke.co.topup.todo.R
 import ke.co.topup.todo.databinding.FragmentTasksBinding
+import ke.co.topup.todo.util.onQueryTextChanged
 
 @AndroidEntryPoint
 class TasksFragment: Fragment(R.layout.fragment_tasks) {
@@ -32,6 +37,36 @@ class TasksFragment: Fragment(R.layout.fragment_tasks) {
         }
         viewModel.tasks.observe(viewLifecycleOwner){
             tasksAdapter.submitList(it)
+        }
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val  searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return  when(item.itemId){
+            R.id.action_sort_by_name -> {
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
