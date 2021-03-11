4509f4e6d9597e6c2df1a91a4544594e55f6acb3
Kibet Theophilus <kibettheophilus@gmail.com>
2021-03-11 09:41:39 +0300

    Implemente Sort Order functionality

--- app/src/main/java/ke/co/topup/todo/data/TaskDao.kt
+++ app/src/main/java/ke/co/topup/todo/data/TaskDao.kt
@@ -1,12 +1,23 @@
 package ke.co.topup.todo.data
 
 import androidx.room.*
+import ke.co.topup.todo.ui.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
-    @Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun  getTasks(searchQuery: String): Flow<List<Task>>
+
+    fun getTasks(query:String, sortOrder: SortOrder, hideCompleted: Boolean):Flow<List<Task>> =
+        when(sortOrder){
+            SortOrder.BY_DATE -> getTasksSortedByDateCreated(query,hideCompleted)
+            SortOrder.BY_NAME -> getTasksSortedByName(query, hideCompleted)
+        }
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
+    fun  getTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created")
+    fun  getTasksSortedByDateCreated(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
 
     @Insert
     suspend fun insert(task: Task)
